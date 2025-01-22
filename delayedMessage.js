bd492fba96994b687736fde0e5e014161b560762
greeshma <baddamgreeshma22@gmail.com>
2025-01-22 22:48:47 +0530

    initial commit

---
+++ delayedMessage.js
@@ -0,0 +1,12 @@
+function delayedMessage(message, delay, callback) {
+    setTimeout(function() {
+      console.log(message);  
+      callback();  
+    }, delay);
+  }
+  
+
+  delayedMessage("Hello, this is a delayed message!", 11200, function() {
+    console.log("The message has been printed.");
+  });
+  
---
+++ greet.js
@@ -0,0 +1,15 @@
+function greet(name, callback) {
+    
+    const message = `Hello, ${name}!`;
+    
+    
+    callback(message);
+}
+
+
+function displayMessage(message) {
+    console.log(message);
+}
+
+
+greet("greeshma", displayMessage);  
